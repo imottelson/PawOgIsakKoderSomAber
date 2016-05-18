3adb92e2c366c9843ddaf94dae7f8da77bbf0375
Jonas Pawlowski Jensen <joje@netcompany.com>
2016-05-18 22:51:22 +0200

    Backpropagation
    First attempt to implement the training of the network using
    backpropagation

---
+++ PawOgIsakKoderSomAber/PawOgIsakKoderSomAber/Interfaces/NeuralNetworkUtil.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace PawOgIsakKoderSomAber.Interfaces
+{
+    abstract class NeuralNetworkUtil : INeuralNetworkUtil
+    {
+        public Vector Evaluate(Vector input, List<Matrix> weights, List<Vector> biases)
+        {
+            return EvaluateWithActivationList(input, weights, biases)[weights.Count + 1];
+        }
+
+        public List<Vector> EvaluateWithActivationList(Vector input, List<Matrix> weights, List<Vector> biases)
+        {
+            int layers = weights.Count + 1;
+            List<Vector> result = new List<Vector>(layers);
+            input.Map(Sigma, input);
+
+            Vector currentLayer = input;
+            for (int i = 0; i < layers; i++)
+            {
+                currentLayer = (Vector)weights[i].Multiply(currentLayer).Add(biases[i]);
+                result[i] = currentLayer;
+                currentLayer.Map(Sigma, currentLayer);
+            }
+            result[layers] = currentLayer;
+            return result;
+        }
+        public abstract double Sigma(double x);
+
+        public abstract double Cost(List<DataPoint> data, List<Matrix> weights, List<Vector> biases);
+
+        public abstract List<Vector> ComputeErrors(DataPoint point, List<Matrix> weights, List<Vector> activations);
+    }
+}
--- PawOgIsakKoderSomAber/PawOgIsakKoderSomAber/NeuralNetwork.cs
+++ PawOgIsakKoderSomAber/PawOgIsakKoderSomAber/NeuralNetwork.cs
@@ -12,47 +12,96 @@ namespace PawOgIsakKoderSomAber
     {
         public double StepSize;
 
-        public int NumberOfUpdates;
-
-        public int BatchSize;
-
-        public List<int> SizezList; 
-
         public List<Vector> BiasesList;
 
-        public List<Matrix> WeightsList; 
+        public List<Matrix> WeightsList;
 
-        public List<DataPoint> TrainingDataList;
-
-        public List<DataPoint> TestDataList;
-
-        public INeuralNetworkUtil Utilities { private get; set; }
+        public NeuralNetworkUtil Utilities { private get; set; }
 
         public Vector Evaulate(Vector input)
         {
             return Utilities.Evaluate(input, WeightsList, BiasesList);
         }
 
-        public void Update(List<DataPoint> batch)
+        public void TrainNetwork(List<DataPoint> batch)
         {
-            throw new NotImplementedException();
+            int batchSize = batch.Count;
+            List<List<Vector>> activationsList = new List<List<Vector>>(batchSize);
+            List<List<Vector>> errorsList = new List<List<Vector>>(batchSize);
+
+            for (int i = 0; i < batchSize; i++)
+            {
+                List<Vector> activations = Utilities.EvaluateWithActivationList(batch[i].Input, WeightsList, BiasesList);
+                activationsList[i] = activations;
+                errorsList[i] = Utilities.ComputeErrors(batch[i], WeightsList, activations);
+            }
+            List<Vector> errorSum = errorsList[0];
+            int errorVectors = errorSum.Count;
+            for (int i = 1; i < batchSize; i++)
+            {
+                for (int j = 0; j < errorVectors; j++)
+                {
+                    errorSum[j] = (Vector)errorSum[j].Add(errorsList[i][j]);
+                }
+            }
+
+            int biasesCount = this.BiasesList.Count;
+            for (int i = 0; i < biasesCount; i++)
+            {
+                this.BiasesList[i].MapIndexed((j, value) => { return value - StepSize*errorSum[i][j]/batchSize; });
+            }
+
+            int weightsCount = this.WeightsList.Count;
+            for (int i = 0; i < weightsCount; i++)
+            {
+                this.WeightsList[i].MapIndexed((n, m, value) =>
+                {
+                    double sum = 0;
+                    for (int j = 0; j < batchSize; j++)
+                    {
+                        sum += errorsList[j][n]*activationsList[j][m];
+                    }
+                    return value - StepSize*sum/batchSize;
+                });
+            }
         }
 
+        public void TrainNetwork(DataPoint point)
+        {
+            List<Vector> activations = Utilities.EvaluateWithActivationList(point.Input, WeightsList, BiasesList);
+            List<Vector> errors = Utilities.ComputeErrors(point, WeightsList, activations);
 
+            int biasesCount = this.BiasesList.Count;
+            for (int i = 0; i < biasesCount; i++)
+            {
+                this.BiasesList[i].MapIndexed((j, value) => { return value - StepSize*errors[i][j]; });
+            }
 
-        public NeuralNetwork()
+            int weightsCount = this.WeightsList.Count;
+            for (int i = 0; i < weightsCount; i++)
+            {
+                this.WeightsList[i].MapIndexed((m,n,value) => { return value - StepSize*errors[i][m]*activations[i][n]; });
+            }
+        }
+
+        private NeuralNetwork()
         {
             this.Utilities = new StandardUtilities();
             this.StepSize = 0.5;
-            this.NumberOfUpdates = 50;
-            this.BatchSize = 30;
         }
 
-        public NeuralNetwork(List<int> sizes) : this()
+        public NeuralNetwork(List<int> layerSizes) : this()
         {
-            //Sets the weights and biases. Not the data
-            throw new NotImplementedException();
+            int layers = layerSizes.Count();
+            List<Vector> biases = new List<Vector>(layers-1);
+            List<Matrix> weights = new List<Matrix>(layers-1);
+            for (int i = 0; i < layers-1; i++)
+            {
+                biases[i] = (Vector)Vector.Build.Random(layerSizes[i + 1]);
+                weights[i] = (Matrix) Matrix.Build.Random(layerSizes[i], layerSizes[i + 1]);
+            }
+            this.WeightsList = weights;
+            this.BiasesList = biases;
         }
-
     }
 }
